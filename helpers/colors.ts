1c68f2f7b90aa2f455b19bfd28bde7a5ef6e7392
Jhony Naranjo <jhonyagus@hotmail.com>
2025-07-06 20:28:36 -0500

    add helpers

---
+++ helpers/colors.ts
@@ -0,0 +1,13 @@
+export const COLORS = {
+  red: "color: red; font-weight: bold;",
+  green: "color: green; font-weight: bold;",
+  blue: "color: blue; font-weight: bold;",
+  yellow: "color: yellow; font-weight: bold;",
+  purple: "color: purple; font-weight: bold;",
+  cyan: "color: cyan; font-weight: bold;",
+  magenta: "color: magenta; font-weight: bold;",
+  gray: "color: gray; font-weight: bold;",
+  black: "color: black; font-weight: bold;",
+  pink: "color: pink; font-weight: bold;",
+  orange: "color: orange; font-weight: bold;",
+};
